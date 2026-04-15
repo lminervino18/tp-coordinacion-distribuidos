ff49bdc6f6f55a23d6684519a4274f4b952dd319
lminervino18 <lminervino@fi.uba.ar>
2026-04-15 04:16:37 -0300

    chore: optimizar message handler y agregar gitignore

--- python/src/gateway/message_handler/message_handler.py
+++ python/src/gateway/message_handler/message_handler.py
@@ -6,20 +6,16 @@ from common import message_protocol
 class MessageHandler:
     def __init__(self):
         self.query_id = str(uuid.uuid4())
-        assert self.query_id
 
     def serialize_data_message(self, message):
         fruit, amount = message
-        assert isinstance(fruit, str)
-        assert fruit
-        assert isinstance(amount, int)
-        assert amount >= 0
 
         internal_message = message_protocol.internal.build_data_message(
             self.query_id,
             fruit,
             amount,
         )
+
         return message_protocol.internal.serialize(internal_message)
 
     def serialize_eof_message(self, message):
@@ -29,17 +25,18 @@ class MessageHandler:
             None,
             [],
         )
+
         return message_protocol.internal.serialize(internal_message)
 
-    def deserialize_result_message(self, message):
-        internal_message = message_protocol.internal.deserialize(message)
+    def deserialize_result_message(self, raw_message):
+        internal_message = message_protocol.internal.deserialize(raw_message)
 
         if not message_protocol.internal.is_final_top_message(internal_message):
             return None
 
-        if message_protocol.internal.get_query_id(internal_message) != self.query_id:
+        query_id = message_protocol.internal.get_query_id(internal_message)
+
+        if query_id != self.query_id:
             return None
 
-        payload = message_protocol.internal.get_payload(internal_message)
-        assert isinstance(payload, list)
-        return payload
+        return message_protocol.internal.get_payload(internal_message)
